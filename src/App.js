eb62bb3568a246b7ef70469da3b3ab9bb26574f7
yousef shallah <yousefmohshallah33@gmail.com>
2019-09-09 13:45:57 +0300

    uplade new antDesgin project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <div className="App" style={{width:400, margin:"auto", marginTop:50}}>
+        <Form className="login-form">
+        <Form.Item>
+        
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />
+        </Form.Item>
+        <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />
+        </Form.Item>
+        <Form.Item>
+        <Form.Item>
+          <Checkbox>Remember me</Checkbox>
+          <a className="login-form-forgot" href="">
+            {" "} Forgot password
+          </a>
+          </Form.Item>
+          <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          </Form.Item>
+        </Form.Item>
+      </Form>
+    </div>
     </div>
   );
 }
